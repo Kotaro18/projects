018fe7cd2d5b68640423a5cd4552568eb0f8b921
Kotaro <irodori5mile18@gmail.com>
2019-10-31 23:37:14 +0900

    add introduce

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User
+  def initialize
+    @first_name = "Kotaro"
+    @last_name = "Tanaka"
+    @birthday = "1988/1/8"
+    @age = 31
+    @birthplace = "Saga"
+    @hobby = "Footsal"
+  end
+
+  def introduce
+    <<~EOS
+
+    私の名前は#{@first_name + @last_name}です。
+    誕生日は#{@birthday}で、年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
+
+    EOS
+  end
+end
